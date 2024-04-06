bff03435019d37c392bb614edf49fe9b53d3269d
coderipper <hello@joaquinsoza.dev>
2024-04-07 01:19:36 +0200

    added types index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,35 @@
+export interface AssetListDescriptor {
+  name: string;
+  provider: string;
+  description: string;
+  icon: string;
+  url: string;
+}
+
+export interface Asset {
+  name: string;
+  contract?: string;
+  code?: string;
+  issuer?: string;
+  org: string;
+  domain: string;
+  icon: string;
+  decimals?: number;
+  comment?: string;
+}
+
+export interface AssetList {
+  name: string;
+  provider: string;
+  description: string;
+  version: string;
+  feedback: string;
+  assets: Asset[];
+}
+
+export function fetchAvailableAssetLists(): Promise<AssetListDescriptor[]>;
+export function fetchAssetList(url: string): Promise<AssetList>;
+export function setAssetListResolverOptions(options: {
+  catalogueUrl: string;
+  ipfsGatewayUrl: string;
+}): void;
